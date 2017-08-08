bbc21cb3fa7a63f69a765e94ab5d886e2beef062
Christophe Hollebeke <c.hollebeke@gmail.com>
2017-08-09 00:32:29 +0200

    Create wc-out-of-stock-variation.php

---
+++ wc-out-of-stock-variation.php
@@ -0,0 +1,17 @@
+add_action( 'woocommerce_after_shop_loop_item', 'wk_out_of_stock_variations_loop' );
+
+function wk_out_of_stock_variations_loop(){
+    global $product;
+    if ( $product->product_type == 'variable' ) {
+
+        $available = $product->get_available_variations();
+        if ( $available )foreach ( $available as $instockitem ) {
+            if ( isset($instockitem['attributes']['attribute_pa_kleur'] ) ) {
+				if ( ( $instockitem['attributes']['attribute_pa_kleur'] == $_GET['filter_kleur'] ) && ( !$instockitem['max_qty']>0 ) ) {
+                    echo  '<p>NIET OP VOORRAAD<p>';
+                }
+            
+            }
+        }
+    }
+}
